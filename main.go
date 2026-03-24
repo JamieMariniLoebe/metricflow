b8d68656c91d227b645b1321a1e9c8925cce6e1a
jamie.loebe <jamie.loebe2@gmail.com>
2026-03-24 17:08:38 -0500

    Initialize Metricflow with Chi router and health endpoint

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	})
+
+	log.Println("MetricFlow starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
