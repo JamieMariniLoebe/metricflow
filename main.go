fa4b99e1842faf33080fdd7cb688d66c90962e89
jamie.loebe <jamie.loebe2@gmail.com>
2026-03-30 23:08:38 -0500

    Wire database initialization and pool into main

--- main.go
+++ main.go
@@ -4,11 +4,37 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/JamieMariniLoebe/metricflow/internal/database"
+	"github.com/JamieMariniLoebe/metricflow/internal/store"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	dbURL := os.Getenv("DATABASE_URL")
+	sourceURL := os.Getenv("SOURCE_URL")
+
+	if dbURL == "" {
+		log.Fatal("Fatal error")
+	}
+
+	if sourceURL == "" {
+		log.Fatal("Fatal error")
+	}
+
+	if err := database.RunMigrations(dbURL, sourceURL); err != nil {
+		log.Fatal("Error")
+	}
+
+	db, err := store.NewPool(dbURL)
+
+	if err != nil {
+		log.Fatal("Error")
+	}
+
+	defer db.Close()
+
 	r := chi.NewRouter()
 
 	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
@@ -18,4 +44,5 @@ func main() {
 
 	log.Println("MetricFlow starting on :8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
