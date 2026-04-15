2776e70cd994fd6349bd7cb47a0ae68f921d60d3
jamie.loebe <jamie.loebe2@gmail.com>
2026-04-15 13:04:38 -0500

    refactor: rename ingester receiver to idiomatic Go convention

--- internal/ingest/ingester.go
+++ internal/ingest/ingester.go
@@ -29,11 +29,11 @@ func NewIngester(s *store.Store, w int) *Ingester {
 	}
 }
 
-func (ingest *Ingester) Start(c context.Context) {
-	for i := 0; i < ingest.workers; i++ {
-		ingest.wait.Go(func() {
-			for item := range ingest.ingest {
-				err := ingest.db.InsertMetric(c, item)
+func (ig *Ingester) Start(c context.Context) {
+	for i := 0; i < ig.workers; i++ {
+		ig.wait.Go(func() {
+			for item := range ig.ingest {
+				err := ig.db.InsertMetric(c, item)
 				if err != nil {
 					slog.Error("Error processing metric insertion", "error", err, "metric_name", item.MetricName)
 				}
@@ -42,16 +42,16 @@ func (ingest *Ingester) Start(c context.Context) {
 	}
 }
 
-func (ingest *Ingester) Submit(m models.Metric) error {
+func (ig *Ingester) Submit(m models.Metric) error {
 	select {
-	case ingest.ingest <- m:
+	case ig.ingest <- m:
 		return nil
 	default:
 		return errors.New("channel full")
 	}
 }
 
-func (ingest *Ingester) Shutdown() {
-	close(ingest.ingest)
-	ingest.wait.Wait()
+func (ig *Ingester) Shutdown() {
+	close(ig.ingest)
+	ig.wait.Wait()
 }
